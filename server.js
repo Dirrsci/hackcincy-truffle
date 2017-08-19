0824f0cc5e000b250c18afd6afc1466864fe8e65
Kevin Mulcrone <kaybesee@gmail.com>
2017-08-19 16:25:42 -0400

    some changes

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
-let abis = process.env.ABIS;
+let abis = JSON.stringify([
+  require('./build/contracts/Event'),
+  require('./build/contracts/EventManager'),
+  require('./build/contracts/Ticket'),
+]);
+
 console.log(abis);
+
+
+server.get('/contract-info', (req, res) => {
+  res.json({
+    abis,
+    terrapinAddr: '???'
+  });
+});
