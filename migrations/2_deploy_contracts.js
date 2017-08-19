07d20734951bfbd07f3c19ed95d291978b975281
Micheal Reeder <micheal@wyzerr.com>
2017-08-19 16:45:07 -0400

    backend builds with contract info

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,12 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var EventManager = artifacts.require("./EventManager.sol");
+let ConvertLib = artifacts.require("./ConvertLib.sol");
+let EventManager = artifacts.require("./EventManager.sol");
 
 module.exports = function(deployer) {
   // deployer.deploy(ConvertLib);
   // deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(EventManager)
     .then(() => {
-      process.env.TERRAPIN_ADDR = EventManager.address;
+      // run server with EventManager's new address
+      require('../server')(EventManager.address);
     });
 };
--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+let restify = require('restify');
+let server = restify.createServer();
+
 let abis = JSON.stringify([
   require('./build/contracts/Event'),
   require('./build/contracts/EventManager'),
-  require('./build/contracts/Ticket'),
+  require('./build/contracts/Ticket')
 ]);
 
-console.log(abis);
-
-
-server.get('/contract-info', (req, res) => {
-  res.json({
-    abis,
-    terrapinAddr: '???'
+// used by truffles deploy process
+module.exports = (terrapinAddr) => {
+  server.get('/contract-info', (req, res, next) => {
+    res.json({
+      abis,
+      terrapinAddr
+    });
+    next();
   });
-});
+
+  server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+  });
+};
